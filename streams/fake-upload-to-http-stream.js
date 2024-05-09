fa7181018bf17bc5162c3fa7cb5f31f22f27cad3
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-05-08 22:46:02 -0300

    feat:brincando com os principais tipos de stream

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,24 @@
+import Readble from 'stream'
+
+class OneToHundredStream extends Readble {
+    index = 1
+
+    _read() {
+        const i = this.index++
+
+        setTimeout(()=> {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+fetch('http://localhost:3334', {
+    method: 'POST',
+    body: new OneToHundredStream()
+})
---
+++ streams/fundamentals.js
@@ -0,0 +1,38 @@
+import { Readable, Writable, Transform } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+    index = 1
+    
+    _read() {
+        const i = this.index++
+
+        setTimeout(()=> {
+            if (i>100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+    
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+class InverseNumberStream extends Transform {
+    _transform(chunck, encoding, callback) {
+        const transformed = Number(chunck.toString())*-1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunck, encoding, callback) {
+        console.log(Number(chunck.toString()) * 10);
+        callback()
+    }
+}
+
+new OneToHundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
