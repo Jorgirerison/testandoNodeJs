f135911eb5b8972d9334a6d0fc32099fe35df0aa
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-06-04 21:49:51 -0300

    feat:criando banco de dados JSON

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import http from 'http'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
 //Criar um usuário (name,email,senha)
 //req chama, res responde
 
-const users = []
+const database = new Database()
 
  const server = http.createServer(async (req, res) => {
     const { method, url} = req
@@ -12,6 +13,8 @@ const users = []
     await json(req, res)
 
     if (method == 'GET' && url == '/users') {
+        const users = database.select('users')
+
         return res
             .end(JSON.stringify(users))
     }
@@ -20,11 +23,13 @@ const users = []
         // desestruturacao
         const {name,email} = req.body
 
-        users.push({
+        const user = {
             id:1,
             name,
             email,
-        })
+        }
+
+        database.insert('users', user)
 
         return res.writeHead(201).end()
     }
