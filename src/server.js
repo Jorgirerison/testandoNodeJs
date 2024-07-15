a34ec95810e0447610d650bdba49369910f6a8f5
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-07-15 20:41:12 -0300

    feat:separando rotas

--- src/server.js
+++ src/server.js
@@ -1,37 +1,21 @@
 import http from 'http'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
 //Criar um usuário (name,email,senha)
 //req chama, res responde
 
-const database = new Database()
-
- const server = http.createServer(async (req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url} = req
 
     await json(req, res)
 
-    if (method == 'GET' && url == '/users') {
-        const users = database.select('users')
+    const route = routes.find(route => {
+        return route.method == method && route.path == url
+    })
 
-        return res
-            .end(JSON.stringify(users))
-    }
-
-    if (method == 'POST' && url == '/users') {
-        // desestruturacao
-        const {name,email} = req.body
-
-        const user = {
-            id:1,
-            name,
-            email,
-        }
-
-        database.insert('users', user)
-
-        return res.writeHead(201).end()
+    if (route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end('Hellow world')
