2f2f75c8963afee379db1a30867cd0818bfc9fad
jorgirerison <jorgipeixoto@alu.ufc.br>
2024-08-06 21:46:40 -0300

    feat:capturando query params

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from '../utils/extract-query-params.js'
 
 //Criar um usuário (name,email,senha)
 //req chama, res responde
@@ -16,8 +17,11 @@ const server = http.createServer(async (req, res) => {
 
     if (route) {
         const routeParams = req.url.match(route.path)
-        // apenas filtrando a variável routeParams para passar apenas o id
-        req.params = { ...routeParams.groups }
+
+        const { query, ...params} = routeParams.groups
+        
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
 
         return route.handler(req, res)
     }
--- utils/build-route-path.js
+++ utils/build-route-path.js
@@ -2,8 +2,8 @@ export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g
     // achar todos e substituir por:
     const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
-    // path regex É a expressão regular
-    const pathRegex = new RegExp(`^${pathWithParams}`)
+
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
     return pathRegex
 }
